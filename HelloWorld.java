1f7c239a0c9a235751f5e4cf511002b10f129f15
reemaamhaz <49565663+reemaamhaz@users.noreply.github.com>
2020-06-23 00:16:02 -0400

    code w/ documentation

--- HelloWorld.java
+++ HelloWorld.java
@@ -9,6 +9,11 @@ import java.lang.*;
 
 public class HelloWorld
 {
+    /*
+        This function prints the statement and name using the command line argument.
+        Parameters: args - the command line arguments
+        Returns: nothing (void)
+    */
     public static void main(String[] args)
     {
        System.out.printf("Hello, %s!\n", args[0]);
